998d1d9385c8fa52e9144e0959b6766138f8ff73
njrgitman-jr <njr.gitman@gmail.com>
2025-12-03 17:49:44 +0300

    update render 2

--- server/index.js
+++ server/index.js
@@ -50,3 +50,51 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`✔ Server running on PORT ${PORT}`));
 
 export default app;
+
+
+
+
+
+// import express from "express";
+// import cors from "cors";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import productRoutes from "./routes/product.routes.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// const isProduction = process.env.NODE_ENV === "production";
+
+// app.use(
+//   cors({
+//     origin: isProduction
+//       ? [/^https:\/\/.*\.vercel\.app$/]
+//       : true,
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+
+// // Test route
+// app.get("/", (req, res) => res.send("🚀 Update-Plan API is running"));
+
+// // Routes
+// app.use("/api/products", productRoutes);
+
+// // MongoDB connection
+// mongoose
+//   .connect(process.env.MONGODB_URI, { dbName: "test" })
+//   .then(() => console.log("✔ MongoDB Connected"))
+//   .catch((err) => console.error("❌ MongoDB Error:", err));
+
+// // Start server
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => console.log(`✔ Server running on PORT ${PORT}`));
+
+// export default app;
+
+
+
